11fc351f9647bd3b4a923aa1f60da20978b96a26
LucasHaug <lucas.haug@hotmail.com>
2023-07-15 15:52:25 +0200

    ♻️ Change where to call xhost

--- docker_build.sh
+++ docker_build.sh
@@ -2,7 +2,5 @@
 
 TAG="stable"
 
-xhost +local:docker
-
 ## Buildando o docker
 docker buildx build . -f Dockerfile.$TAG -t thunderatz/vss-league-env:$TAG
--- docker_run.sh
+++ docker_run.sh
@@ -4,6 +4,8 @@ TAG="stable"
 
 DOCKER_IMAGE="thunderatz/vss-league-env:$TAG"
 
+xhost +local:docker
+
 docker run -it \
            --rm \
            --privileged \
