f0b373181994767a47c5751ef17de7d371a52180
MatOrofino <matheus.oro6@gmail.com>
2026-06-11 10:22:16 -0300

    :pencil2: fix tag in docker_build and docker_run
    Fix all typos pointed by Bernardo in last PR

--- docker_run.sh
+++ docker_run.sh
@@ -1,17 +1,16 @@
 #!/usr/bin/env bash
 
-TAG="headless"
+TAG="stable"
 
 DOCKER_IMAGE="thunderatz/vss-league-env:$TAG"
-
-SLEEP="30"
 
 xhost +local:docker
 
 docker run -it \
-           --rm \
-           --name vsss-gui \
-           --ipc=host \
-           -e DISPLAY=$DISPLAY \
-           -v /tmp/.X11-unix:/tmp/.X11-unix \
-           $DOCKER_IMAGE 
+  --rm \
+  --name vsss-gui \
+  --ipc=host \
+  -e DISPLAY=$DISPLAY \
+  -v /tmp/.X11-unix:/tmp/.X11-unix \
+  $DOCKER_IMAGE
+
